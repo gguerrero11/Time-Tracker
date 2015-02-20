d5804bfd987d1f17a91b6aae676a332f32cd2b77
Gamaliel Tellez <gamatellez@gmail.com>
2015-02-20 15:37:31 -0700

    "Cmplete Step 4"

---
+++ Time-Tracker/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  Time-Tracker
+//
+//  Created by Gamaliel Tellez on 2/20/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *titleTextField;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UITableView *entryTableView;
+
+@end
